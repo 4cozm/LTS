7761fa974fb130dcc23cf726a472baa0b6664d11
4cozm <17328rm@gmail.com>
2025-04-08 16:01:06 +0900

    환경 설정 파일 추가 및 Azure Key Vault 연동 코드 수정

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Antiforgery;
+using Azure.Identity;
+using DotNetEnv;
 
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
+var envVar = Environment.GetEnvironmentVariable("ENV");
+Console.WriteLine($"🌱 현재 빌드 환경: {envVar}");
+
+// Azure Key Vault 연동
+builder.Configuration.AddAzureKeyVault(
+    new Uri("https://ltsdevkey.vault.azure.net/"),
+    new DefaultAzureCredential());
+
+// Razor Pages 설정 및 Antiforgery
 builder.Services.AddRazorPages(options =>
 {
     options.Conventions.ConfigureFilter(new AutoValidateAntiforgeryTokenAttribute());
 });
 builder.Services.AddAntiforgery();
+
 var app = builder.Build();
 app.UseStaticFiles();
-app.MapRazorPages();
 app.UseRouting();
-
+app.MapRazorPages();
 app.Run();
-
